f97bc96a6739d5174b15152a41b86b4ec158349e
UbVova <ubvova@local.tomcat.ru>
2021-11-17 15:18:09 +0300

    Fix lazy initialization subscription for user.

--- src/main/java/ru/lesson/springBootProject/controllers/GreetingController.java
+++ src/main/java/ru/lesson/springBootProject/controllers/GreetingController.java
@@ -1,13 +1,17 @@
 package ru.lesson.springBootProject.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.persistence.EntityManager;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    EntityManager em;
     @GetMapping("/")
     public String root( Map<String, Object> map,
                         @RequestParam(value = "message",required = false)String message){
